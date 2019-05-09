c12897bb90bb69db222e6bde634af1c3b6b351b6
Dave <redirwin@gmail.com>
2019-05-08 22:43:20 -0700

    Completed prototype assignment. MVP completed.

--- assignments/this.js
+++ assignments/this.js
@@ -1,66 +1,60 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 'Global' binding is the default context. In a browser, the value of 'this' will be the window or console itself.
+* 1. GLOBAL binding is the default context. In a browser, the value of 'this' will be the window or console itself where all of JavaScript lives.
 
-* 2. In 'implicit' binding, 'this' refers to the object that comes before the dot. We call the function with . notation after the object's name, and the 'this' in the function refers to the object, allowing us access to the properties and functions of that object.
+* 2. In IMPLICIT binding, 'this' refers to the object that comes before the dot. We call the function with . notation after the object's name, and the 'this' in the function refers to the object, allowing us access to the properties and functions of that object.
 
-* 3. The 'new' binding is used to create an object that inherits the properties of the prototype. The 'this' in the prototype refers specifically to that instance of the new object.
+* 3. The NEW binding is used to create an object that inherits the properties of the prototype. The 'this' in the prototype refers specifically to new object.
 
-* 4. With 'explicit' binding, we directly tell a function what object to use for 'this.' We use built-in functions like 'call', 'apply', and 'bind' to pass the object as an argument to the function. 
+* 4. With EXPLICIT binding, we directly tell a function what object to use for 'this' and it can be different than the object who's method we are using. We use built-in functions like 'call', 'apply', and 'bind' to pass a different object as a parameter into the original object's method that we're calling. The 'this' applies to the new object. 
 
 
-* write out a code example of each explanation above
+* write out a code example of each explanation above*/
 
-*/
+// Principle 1: Window Binding
+const showJavaScript = this;
+console.log(showJavaScript);
 
-// Principle 1
-
-// code example for Window Binding
-
-function windowBinding() {
-    console.log(this);
-}
-windowBinding();
-
-// Principle 2
-
-// code example for Implicit Binding
-
+// Principle 2: Implicit Binding
 const aboutMe = {
-    greet: "Hello, there!",
+    greet: "Hey there!",
     introduction: function (name, profession) {
         console.log(`${this.greet} My name is ${name} and I'm a ${profession}.`)
     }
 }
 aboutMe.introduction('Dave', 'software developer');
 
-// Principle 3
-
-// code example for New Binding
-
-function imHungry(time, food) {
+// Principle 3: New Binding
+function Meal(time, food) {
     this.time = time;
     this.food = food;
     this.letsEat = function () {
-        console.log(`It's already ${this.time}! I want some ${this.food}.`)
+        console.log(`It's ${this.time}! I want some ${this.food}.`)
+    }
+}
+const lunch = new Meal('2pm', 'french fries');
+const dinner = new Meal('almost midnight', 'hot wings and pizza')
+lunch.letsEat();
+dinner.letsEat();
+
+// Principle 4: Explicit Binding
+const myDog = {
+    type: "dog",
+    name: "Birdie",
+    breed: "border collie",
+    sex: "female",
+    allAbout: function () {
+        console.log(`My ${this.type} is a ${this.sex} ${this.breed} named ${this.name}.`)
     }
 }
 
-const meal = new imHungry('2pm', 'french fries');
-
-meal.letsEat();
-
-// Principle 4
-
-// code example for Explicit Binding
-
-function difficulty() {
-    console.log(this.level);
+const myCat = {
+    type: "cat",
+    name: "Bear",
+    breed: "Maine Coon",
+    sex: "male"
 }
 
-let howHard = {
-    level: "This JS 'this' stuff is breaking my brain."
-}
-
-difficulty.call(howHard);
+myDog.allAbout();
+myDog.allAbout.call(myCat);
